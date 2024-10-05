579175992cbf0349fab63598a1bf553b12532e66
piya07 <piyushkumbhar07@gmail.com>
2024-10-05 18:47:43 +0530

    max

---
+++ index.js
@@ -0,0 +1,46 @@
+let text = "maxop"
+let vale = text.length;
+let valee = text.toUpperCase()
+console.log(valee)
+
+let text1 = "rtr"
+let value = text1.charAt(1)
+console.log(value)
+
+let text2 = "maxtomin"
+let val = text2.indexOf('i')
+console.log(val)
+
+const text3 = "maxtomin"
+const va = text3[2];
+console.log(va)
+
+let text4 = "maxtomin"
+let vall = text4.slice(2,-5)
+console.log(vall)
+
+let text5 = "maxtouin"
+let valll = text5.substring(2,10)
+console.log(valll)
+
+let a = "max"
+let b = "min"
+let c = a.concat(b)
+console.log(c)
+
+let u = "    piyush"
+let y = u.trim()
+console.log(y)
+
+let p = "piyush"
+let q = p.repeat(2)
+console.log(q)
+
+let x = "piyush,home"
+let z = x.replace("home","kumbhar")
+console.log(z)
+
+let d = "piyush"
+let e = d.split("")
+console.log(e)
+
